d334f436b52b38b01520b83ddef5ba24de518faf
Coleman Buffa <75283364+coleman-buffa@users.noreply.github.com>
2023-07-10 12:36:02 -0700

    Initialize project repository

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NestedLayout.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Automation()
+        {
+            return View();
+        }
+
+        public ActionResult Electronics()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NestedLayout.MvcApplication" Language="C#" %>
